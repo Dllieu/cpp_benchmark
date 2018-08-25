0344dc19290c1fbdc7312a7aea45923e5430dc68
Dllieu <molina.stephan@gmail.com>
2018-08-25 17:28:31 +0800

    cmake cleaning

--- cmake/external.cmake
+++ cmake/external.cmake
@@ -1,4 +1,3 @@
 set(PATH_FLAT_HASH_MAP "${CMAKE_CURRENT_SOURCE_DIR}/external/flat_hash_map")
 set(PATH_GOOGLE_BENCHMARK "${CMAKE_CURRENT_SOURCE_DIR}/external/benchmark")
 set(PATH_GOOGLE_TEST "${CMAKE_CURRENT_SOURCE_DIR}/external/googletest")
-set(PATH_LIB_EXPERIMENTAL "${CMAKE_CURRENT_SOURCE_DIR}/experimental/experimental")
