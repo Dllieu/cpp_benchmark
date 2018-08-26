ab01c7fb87130b385191a6f45a9ad20f8ac5c574
Dllieu <molina.stephan@gmail.com>
2018-08-26 23:00:36 +0800

    fix build

--- appveyor/upload_googlebenchmark_result.py
+++ appveyor/upload_googlebenchmark_result.py
@@ -7,15 +7,15 @@ def setup_args():
     parser._action_groups.pop()
 
     requiredArguments = parser.add_argument_group('required arguments')
-    requiredArguments.add_argument("--googlebenchmark_xml", help="XML filename from googlebenchmark output", required=True)
+    requiredArguments.add_argument("--googlebenchmark_csv", help="CSV filename from googlebenchmark output", required=True)
 
     optionalArguments = parser.add_argument_group('optional arguments')
     optionalArguments.add_argument("--debug", action='store_true', help="Echo output instead of sending to appveyor")
 
     return parser.parse_args()
 
-def find_xml_header_index(xmlFile):
-    with open(xmlFile) as f:
+def find_csv_header_index(csvFile):
+    with open(csvFile) as f:
         index = 0
         for line in f.readlines():
             if line.startswith('name,iterations'):
@@ -23,12 +23,12 @@ def find_xml_header_index(xmlFile):
 
             index += 1
 
-    raise Exception('No header found! [File={}]'.format(xmlFile))
+    raise Exception('No header found! [File={}]'.format(csvFile))
 
 if __name__ == "__main__":
     args = setup_args()
 
-    bla = pd.read_csv(args.googlebenchmark_xml, header=find_xml_header_index(args.googlebenchmark_xml))
+    bla = pd.read_csv(args.googlebenchmark_csv, header=find_csv_header_index(args.googlebenchmark_csv))
     print(bla)
 
     subprocess.call('echo appveyor PushArtifact BLABLA.JPG', shell=True)
