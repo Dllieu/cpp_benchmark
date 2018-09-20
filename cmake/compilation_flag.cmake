01e7c305b7dd93e17d42b475b1ae38c86232460c
Dllieu <molina.stephan@gmail.com>
2018-09-20 13:01:04 +0800

    appveyor: upgrade to ubuntu 18

--- cmake/compilation_flag.cmake
+++ cmake/compilation_flag.cmake
@@ -3,8 +3,8 @@ set(COMPILER_FLAGS_WARNING "-Werror -Wall -Wextra -Wno-deprecated-declarations -
                             -Wdeprecated-declarations -Wnon-virtual-dtor -Wcast-align -Woverloaded-virtual -Wabi -Wcast-qual \
                             -Wno-unused-local-typedefs -Wno-unused-parameter")
 
-# -fsanitize=undefined fail on appveyor (16.04)
-set(CMAKE_CXX_FLAGS_DEBUG "-O0 -D_DEBUG -fsanitize=address -fsanitize=leak -fno-sanitize=alignment -fno-inline")
+set(CMAKE_CXX_FLAGS_DEBUG "-O0 -D_DEBUG -fno-inline \
+                           -fsanitize=address -fsanitize=leak -fsanitize=undefined -fno-sanitize=alignment")
 
 # Disable '-fomit-frame-pointer' during profiling
 set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG -march=native -fomit-frame-pointer")
@@ -14,13 +14,13 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
 
     # gcov
     set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fprofile-arcs -ftest-coverage")
-    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -fdevirtualize-at-ltrans -floop-interchange -floop-strip-mine -floop-block -fgraphite-identity -funsafe-loop-optimizations -static-libstdc++")
 
-    # # Bin utils issue with gcc 8 on 16.04
-    # # -flto -fno-fat-lto-objects -Wno-odr
-    # if (${CMAKE_BUILD_TYPE} STREQUAL "Release")
-    #         SET(BENCHMARK_ENABLE_LTO ON CACHE BOOL "Build google benchmark with lto")
-    # endif()
+    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -fdevirtualize-at-ltrans -floop-interchange -floop-strip-mine -floop-block \
+                                                            -fgraphite-identity -funsafe-loop-optimizations -static-libstdc++ \
+                                                            -flto -fno-fat-lto-objects -Wno-odr")
+    if (${CMAKE_BUILD_TYPE} STREQUAL "Release")
+            SET(BENCHMARK_ENABLE_LTO ON CACHE BOOL "Build google benchmark with lto")
+    endif()
 elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     # Clang is only used for additionals warnings and usage of the sanitizers
     # (Not interested by llvm libc++ at the moment)
